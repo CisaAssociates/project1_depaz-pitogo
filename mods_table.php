a367d525437fa6f2d420cbe3dee609c9b85b898f
CisaAssociates <associatecisa@gmail.com>
2025-05-05 14:26:42 +0800

    Update mods_table.php

--- mods_table.php
+++ mods_table.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "", "attendance_system");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('config.php');
 
 // 👉 Handle logout
 if (isset($_POST['logout'])) {
