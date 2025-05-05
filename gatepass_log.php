c3874e1bcbd125809fdc63067e68f91731fffda7
CisaAssociates <associatecisa@gmail.com>
2025-05-05 14:25:41 +0800

    Update gatepass_log.php

--- gatepass_log.php
+++ gatepass_log.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "", "attendance_system");
+$conn = new mysqli("localhost", "u347279731_depaz_pitogo", "DepazPitogo2025", "u347279731_depaz_pitogodb");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
