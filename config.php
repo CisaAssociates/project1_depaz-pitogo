95a9dea48bf8f011a1c7c2c34980dc494ffa32ca
CisaAssociates <associatecisa@gmail.com>
2025-05-05 13:35:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 
 // Database connection
-$conn = new mysqli("localhost", "root", "", "attendance_system");
+$conn = new mysqli("localhost", "u347279731_depaz_pitogo", "DepazPitogo2025", "u347279731_depaz_pitogodb");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
